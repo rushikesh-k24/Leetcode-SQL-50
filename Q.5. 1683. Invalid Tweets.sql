b00e6222fcea290dc556d61da437be361302bdf1
rushikesh-k24 <rushikolhe04@gmail.com>
2026-01-28 19:08:11 +0530

    Q.5. 1683. Invalid Tweets.sql

---
+++ Q.5. 1683. Invalid Tweets.sql
@@ -0,0 +1,5 @@
+# Problem statement
+'''Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15'''
+
+select tweet_id from Tweets
+where length(content) > 15
