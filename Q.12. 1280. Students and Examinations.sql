37a5c733845c4c0f785fcf1be23debd47a095924
rushikesh-k24 <rushikolhe04@gmail.com>
2026-01-29 21:17:43 +0530

    Q.12. 1280. Students and Examinations.sql

---
+++ Q.12. 1280. Students and Examinations.sql
@@ -0,0 +1,15 @@
+-- Write a solution to find the number of times each student attended each exam. Return the result table ordered by student_id and subject_name.
+
+
+SELECT
+    S.student_id
+    ,S.student_name
+    ,Su.subject_name
+    ,COUNT(E.student_id) attended_exams
+FROM Students S
+CROSS JOIN Subjects Su
+LEFT JOIN Examinations E
+    ON S.student_id = E.student_id
+    AND Su.subject_name = E.subject_name
+GROUP BY S.student_id, S.student_name, Su.subject_name
+ORDER BY S.student_id, S.student_name, Su.subject_name
